b277b7aa15e77b92fddb929e48cad1cac931690d
Sebastian Christiansen <seba4746@stud.kea.dk>
2019-05-15 17:19:45 +0200

    Added unittest for Deck class. Testing for make_suit() now possible

---
+++ DeckTests.py
@@ -0,0 +1,14 @@
+
+import unittest
+from Deck import Deck
+
+def test_make_suit(deck, suit, exp_suit):
+    assert deck.make_suit([], suit)[0].suit == exp_suit, "Should be " + exp_suit
+
+
+if __name__ == "__main__":
+    deck = Deck([])
+
+    deck.cards = []
+
+    test_make_suit(deck, 'Spades', 'Hearts') 
