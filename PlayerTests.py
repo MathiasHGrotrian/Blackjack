c1268cadb5cb973774527c0a9f3cbc75fe573afa
Sebastian Christiansen <seba4746@stud.kea.dk>
2019-05-15 17:06:04 +0200

    Added more tests for player class. Improved methods by giving them dynamic expected values instead of set ones

--- PlayerTests.py
+++ PlayerTests.py
@@ -5,12 +5,26 @@ from Card import Card
 
 
 
-def test_calc_total_rank(player):
+def test_calc_total_rank(player, exp_total):
     
-    assert player.calculate_total_rank() == 21, 'Should be 21'
+    assert player.calculate_total_rank() == exp_total, 'Should be ' + str(exp_total)
+
+def test_check_rank_aces(player, card, exp_rank):
+
+    assert player.check_rank(card) == exp_rank, 'Should be ' + str(exp_rank)
+
+def test_check_rank_face_cards(player, card, exp_rank):
+
+    assert player.check_rank(card) == exp_rank , 'Should be ' + str(exp_rank)
 
 
 if __name__ == "__main__":
-    player = Player('Player', 1000, [Card("h",10),Card("h",1) ])
 
-    test_calc_total_rank(player)
+    ace_card = Card('Heart', 'Ace')
+    jack_card = Card('Heart', 'Jack')
+
+    player = Player('Player', 1000, [Card('Heart', 10),Card('Heart', 5) ])
+
+    test_calc_total_rank(player, 15)
+    test_check_rank_aces(player, ace_card, 1)
+    test_check_rank_face_cards(player, jack_card, 10)
