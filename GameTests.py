80b8a85b62b9ece2ad4970720ca5c56d493e8675
MathiasHGrotrian <math524s@stud.kea.dk>
2019-05-22 11:38:53 +0200

    added test for place bet

--- GameTests.py
+++ GameTests.py
@@ -9,7 +9,7 @@ def test_evaluate_win_condition(player, dealer, choice, exp_bool):
     assert Game.evaluate_win_condition(player, dealer, choice) == exp_bool, 'Should be ' + str(exp_bool)
 
 def test_place_bet(player, current_pool, exp_int):
-    assert Game.place_bet(player, current_pool) == exp_int, 'S'
+    assert Game.place_bet(player, current_pool) == exp_int, 'Should be ' + str(exp_int)
 
 
 if __name__ == "__main__":
@@ -17,3 +17,4 @@ if __name__ == "__main__":
     dealer = Player('Dealer', 1000, [Card('Spades', 10),Card('Hearts', 2) ])
 
     test_evaluate_win_condition(player, dealer, '2', True)
+    test_place_bet(player, 0, 50)
